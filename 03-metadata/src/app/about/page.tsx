f5b69419d8b396f63f06177015d48ec4e1590005
iamharshtambe <harshtambe.dev@gmail.com>
2025-08-05 19:13:09 +0530

    add static metadata object, generateMetadata function and metadata in client component

---
+++ 03-metadata/src/app/about/page.tsx
@@ -0,0 +1,11 @@
+export const metadata = {
+  title: 'About Metadta',
+};
+
+export default function About() {
+  return (
+    <div>
+      <h1 className="font-bold text-2xl text-center">About</h1>
+    </div>
+  );
+}
