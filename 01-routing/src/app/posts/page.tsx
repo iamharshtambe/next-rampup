6dc0b06ebd341b713af47be8a8263c799c557486
iamharshtambe <harshtambe.dev@gmail.com>
2025-08-06 18:32:58 +0530

    add dynamic link

--- 01-routing/src/app/posts/page.tsx
+++ 01-routing/src/app/posts/page.tsx
@@ -1,7 +1,22 @@
+import Link from 'next/link';
+
 export default function Post() {
   return (
     <div>
       <h1 className="font-bold text-2xl text-center">Posts</h1>
+
+      <p className="text-center">
+        <Link href="/">Go to Home </Link>
+      </p>
+
+      <div className="text-center">
+        <Link href="/posts/1" replace>
+          <p>Post 1</p>
+        </Link>
+        <Link href="/posts/1">
+          <p>Post 2</p>
+        </Link>
+      </div>
     </div>
   );
 }
