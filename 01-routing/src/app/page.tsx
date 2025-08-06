fa6f281ceb03ced6cb2b04499558965855ea7379
iamharshtambe <harshtambe.dev@gmail.com>
2025-08-06 15:33:21 +0530

    add link to all routes

--- 01-routing/src/app/page.tsx
+++ 01-routing/src/app/page.tsx
@@ -1,7 +1,27 @@
+import Link from 'next/link';
+
 export default function Home() {
   return (
     <div>
-      <h1 className="font-bold text-2xl text-center">Home</h1>
+      <Link href="/">
+        <h1 className="font-bold text-2xl text-center">Home</h1>
+      </Link>
+
+      <Link href="/profile">
+        <h2 className="font-bold text-2xl text-center">Profile</h2>
+      </Link>
+
+      <Link href="/blogs">
+        <h2 className="font-bold text-2xl text-center">Blogs</h2>
+      </Link>
+
+      <Link href="/posts">
+        <h2 className="font-bold text-2xl text-center">Posts</h2>
+      </Link>
+
+      <Link href="/products">
+        <h2 className="font-bold text-2xl text-center">Products</h2>
+      </Link>
     </div>
   );
 }
