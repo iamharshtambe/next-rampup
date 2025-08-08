59d5f7069608c105ede4d085081215e1edcfbbaa
iamharshtambe <harshtambe.dev@gmail.com>
2025-08-08 15:05:35 +0530

    handle errors

--- 01-routing/src/app/posts/[postId]/impressions/[impressionId]/page.tsx
+++ 01-routing/src/app/posts/[postId]/impressions/[impressionId]/page.tsx
@@ -4,6 +4,15 @@ export default async function ImpressionDetails({
   params: Promise<{ postId: string; impressionId: string }>;
 }) {
   const { postId, impressionId } = await params;
+  function getRandom(count: number) {
+    return Math.floor(Math.random() * count);
+  }
+
+  const random = getRandom(2);
+
+  if (random === 1) {
+    throw new Error('Error loading impressions');
+  }
   return (
     <div>
       <h1 className="font-bold text-2xl text-center">Impression Details</h1>
