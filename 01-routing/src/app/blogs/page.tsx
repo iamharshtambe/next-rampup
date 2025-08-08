a6a177b91f59fa7f451dfa051d75b01581679fd2
iamharshtambe <harshtambe.dev@gmail.com>
2025-08-08 13:42:09 +0530

    add loading file

--- 01-routing/src/app/blogs/page.tsx
+++ 01-routing/src/app/blogs/page.tsx
@@ -1,4 +1,9 @@
-export default function Blog() {
+export default async function Blog() {
+  await new Promise((resolve) => {
+    setTimeout(() => {
+      resolve('Delay');
+    }, 2000);
+  });
   return (
     <div>
       <h1 className="font-bold text-2xl text-center">Blog</h1>
