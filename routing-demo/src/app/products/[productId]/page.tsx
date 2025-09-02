a276f79ded4cafa63c6221aec9eca8176fc622cb
iamharshtambe <harshtambe.dev@gmail.com>
2025-09-02 22:38:09 +0530

    add dynamic generate metadata function

--- routing-demo/src/app/products/[productId]/page.tsx
+++ routing-demo/src/app/products/[productId]/page.tsx
@@ -1,8 +1,20 @@
-export default async function ProductDetails({
-  params,
-}: {
+import { Metadata } from 'next';
+
+type ParamsType = {
   params: Promise<{ productId: string }>;
-}) {
+};
+
+export async function generateMetadata({
+  params,
+}: ParamsType): Promise<Metadata> {
+  const { productId } = await params;
+
+  return {
+    title: `Product ${productId}`,
+  };
+}
+
+export default async function ProductDetails({ params }: ParamsType) {
   const { productId } = await params;
 
   return <div>Product Details - {productId}</div>;
