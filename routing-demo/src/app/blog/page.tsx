2cc1354203d8ade4f5aada60c4b5e03b4926b22e
iamharshtambe <harshtambe.dev@gmail.com>
2025-09-03 21:59:08 +0530

    add loading ui

--- routing-demo/src/app/blog/page.tsx
+++ routing-demo/src/app/blog/page.tsx
@@ -1,3 +1,9 @@
-export default function Blog() {
+export default async function Blog() {
+  await new Promise((resolve) => {
+    setTimeout(() => {
+      resolve('Delay');
+    }, 2000);
+  });
+
   return <div>Blog</div>;
 }
