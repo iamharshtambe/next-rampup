7847b3d5a8ceeea8a5ab19b540bb6f61635fa9f8
iamharshtambe <harshtambe.dev@gmail.com>
2025-09-03 21:52:16 +0530

    add template

--- routing-demo/src/app/(auth)/template.tsx
+++ routing-demo/src/app/(auth)/template.tsx
@@ -3,6 +3,7 @@
 import Link from 'next/link';
 import '../globals.css';
 import { usePathname } from 'next/navigation';
+import { useState } from 'react';
 
 const navLinks = [
   { name: 'Register', href: '/register' },
@@ -16,9 +17,17 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   const pathname = usePathname();
+  const [input, setInput] = useState('');
 
   return (
     <div className="h-[600px] p-4 flex flex-col">
+      <input
+        type="text"
+        value={input}
+        onChange={(e) => setInput(e.target.value)}
+        className="border border-black w-36 px-2 py-1"
+      />
+
       {navLinks.map((link, index) => {
         const isActive =
           pathname === link.href ||
