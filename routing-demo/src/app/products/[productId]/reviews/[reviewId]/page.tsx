5545011efcb2a667e50d428d9a9edb002a676cc6
iamharshtambe <harshtambe.dev@gmail.com>
2025-09-03 22:15:34 +0530

    handle errors in review

--- routing-demo/src/app/products/[productId]/reviews/[reviewId]/page.tsx
+++ routing-demo/src/app/products/[productId]/reviews/[reviewId]/page.tsx
@@ -1,10 +1,19 @@
 import { notFound } from 'next/navigation';
 
+function generateRandomNumber(count: number) {
+  return Math.floor(Math.random() * count);
+}
+
 export default async function ReviewDetails({
   params,
 }: {
   params: Promise<{ productId: string; reviewId: string }>;
 }) {
+  const randomError = generateRandomNumber(2);
+
+  if (randomError === 1) {
+    throw new Error('Error loading review');
+  }
   const { productId, reviewId } = await params;
 
   if (parseInt(reviewId) > 1000) {
