734174f44876172719852fd1251ff766b4629a2c
iamharshtambe <harshtambe.dev@gmail.com>
2025-09-05 18:47:37 +0530

    work with cookies

--- route-handlers-demo/src/app/profile/api/route.ts
+++ route-handlers-demo/src/app/profile/api/route.ts
@@ -1,4 +1,4 @@
-import { headers } from 'next/headers';
+import { cookies, headers } from 'next/headers';
 import { NextRequest } from 'next/server';
 
 export async function GET(request: NextRequest) {
@@ -7,7 +7,15 @@ export async function GET(request: NextRequest) {
 
   const requestHeaders = await headers();
   console.log(requestHeaders.get('Authorization'));
+
+  const theme = request.cookies.get('theme');
+  console.log(theme);
+
+  const cookieStore = await cookies();
+  cookieStore.set('hero', 'zero');
+  console.log(cookieStore.get('hero'));
+
   return new Response('<h1>Prodile API</h1>', {
-    headers: { 'content-type': 'text/html' },
+    headers: { 'content-type': 'text/html', 'Set-Cookie': 'theme=dark' },
   });
 }
