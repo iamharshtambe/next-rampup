66881c37f94827b61f5c3905d8df6579eca5a5df
iamharshtambe <harshtambe.dev@gmail.com>
2025-09-05 12:06:22 +0530

    add get request

---
+++ route-handlers-demo/src/app/comments/data.ts
@@ -0,0 +1,5 @@
+export const comments = [
+  { id: 1, comment: 'This is the first comment.' },
+  { id: 2, comment: "Here's another insightful comment." },
+  { id: 3, comment: 'Great post! Thanks for sharing.' },
+];
