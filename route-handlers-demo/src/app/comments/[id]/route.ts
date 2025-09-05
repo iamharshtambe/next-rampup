b9597a538180cd57f675f1bb25cf57b8094dc6cc
iamharshtambe <harshtambe.dev@gmail.com>
2025-09-05 14:15:06 +0530

    add delete request

--- route-handlers-demo/src/app/comments/[id]/route.ts
+++ route-handlers-demo/src/app/comments/[id]/route.ts
@@ -20,3 +20,14 @@ export async function PATCH(
   comments[index].comment = comment;
   return Response.json(comments[index]);
 }
+
+export async function DELETE(
+  request: Request,
+  { params }: { params: Promise<{ id: string }> }
+) {
+  const { id } = await params;
+  const index = comments.findIndex((c) => c.id === parseInt(id));
+  const deletedComment = comments[index];
+  comments.splice(index, 1);
+  return Response.json(deletedComment);
+}
