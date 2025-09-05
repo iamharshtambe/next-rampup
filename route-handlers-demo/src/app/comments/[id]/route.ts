d61ee5052bbeced7ad6fec8316d3e5514101cffb
iamharshtambe <harshtambe.dev@gmail.com>
2025-09-05 13:59:23 +0530

    add patch request

--- route-handlers-demo/src/app/comments/[id]/route.ts
+++ route-handlers-demo/src/app/comments/[id]/route.ts
@@ -8,3 +8,15 @@ export async function GET(
   const comment = comments.find((c) => c.id === parseInt(id));
   return Response.json(comment);
 }
+
+export async function PATCH(
+  request: Request,
+  { params }: { params: Promise<{ id: string }> }
+) {
+  const { id } = await params;
+  const updatedData = await request.json();
+  const { comment } = updatedData;
+  const index = comments.findIndex((c) => c.id === parseInt(id));
+  comments[index].comment = comment;
+  return Response.json(comments[index]);
+}
