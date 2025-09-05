a27e7b24467e1859308d87ac005c0c992590ae7b
iamharshtambe <harshtambe.dev@gmail.com>
2025-09-05 21:22:28 +0530

    implement middleware

---
+++ route-handlers-demo/src/middleware.ts
@@ -0,0 +1,11 @@
+import { NextResponse } from 'next/server';
+import type { NextRequest } from 'next/server';
+
+export function middleware(request: NextRequest) {
+  const response = NextResponse.next();
+  const themePreference = request.cookies.get('theme');
+  if (!themePreference) {
+    response.cookies.set('theme', 'dark');
+  }
+  return response;
+}
