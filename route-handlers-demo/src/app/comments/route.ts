d4690f6fefe902df14442feb9711295fb303a2eb
iamharshtambe <harshtambe.dev@gmail.com>
2025-09-05 12:19:06 +0530

    add post request

--- route-handlers-demo/src/app/comments/route.ts
+++ route-handlers-demo/src/app/comments/route.ts
@@ -3,3 +3,10 @@ import { comments } from './data';
 export async function GET() {
   return Response.json(comments);
 }
+
+export async function POST(request: Request) {
+  const comment = await request.json();
+  const newComment = { id: comments.length + 1, comment: comment.comment };
+  comments.push(newComment);
+  return Response.json(newComment, { status: 201 });
+}
