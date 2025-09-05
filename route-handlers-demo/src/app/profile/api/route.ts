840284f557cceeabeca324458f693b6d98651568
iamharshtambe <harshtambe.dev@gmail.com>
2025-09-05 18:34:13 +0530

    add header in route handlers

--- route-handlers-demo/src/app/profile/api/route.ts
+++ route-handlers-demo/src/app/profile/api/route.ts
@@ -1,3 +1,13 @@
-export async function GET() {
-  return new Response('This is a Profile');
+import { headers } from 'next/headers';
+import { NextRequest } from 'next/server';
+
+export async function GET(request: NextRequest) {
+  // const requestHeaders = new Headers(request.headers);
+  // console.log(requestHeaders.get('Authorization'));
+
+  const requestHeaders = await headers();
+  console.log(requestHeaders.get('Authorization'));
+  return new Response('<h1>Prodile API</h1>', {
+    headers: { 'content-type': 'text/html' },
+  });
 }
