5dc497f46e9c05fd93a4878c985854994e0758ad
iamharshtambe <harshtambe.dev@gmail.com>
2025-09-06 21:31:43 +0530

    fix bug in sequential data fetching

--- data-fetching-demo/src/app/sequential-fetching/page.tsx
+++ data-fetching-demo/src/app/sequential-fetching/page.tsx
@@ -11,11 +11,13 @@ export default async function SqeuentialFetching() {
   const res = await fetch('https://jsonplaceholder.typicode.com/posts');
   const posts: Post = await res.json();
 
+  const filteredPosts = posts.filter((post) => post.id % 10 === 1);
+
   return (
     <div className="p-4">
       <h1 className="text-2xl font-bold">Squential Data Fetching</h1>
       <div>
-        {posts.map((post) => (
+        {filteredPosts.map((post) => (
           <div
             key={post.id}
             className="border border-white p-2 rounded-2xl my-4"
